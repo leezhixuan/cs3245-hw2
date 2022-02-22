745a22f1f65d8db8a207d2065368e230cc219386
zhixuanlee <overlypositiveplayer@gmail.com>
2022-02-22 20:10:15 +0800

    Add Node class

---
+++ Node.py
@@ -0,0 +1,20 @@
+from pydoc import doc
+
+
+class Node(object):
+
+    def __init__(self, docID):
+        self.docID = docID
+        self.skipPointer = 0 # target index = skipPointer + currentIndex
+
+    def addSkipPointer(self, value):
+        self.skipPointer = value
+
+    def hasSkip(self):
+        return self.skipPointer != 0
+        
+    def __str__(self):
+        return str(self.docID)
+
+    def getDocID(self):
+        return self.docID
