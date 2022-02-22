207d6dddc7afb1be407f6138c3f4381a9d325d31
zhixuanlee <overlypositiveplayer@gmail.com>
2022-02-23 01:09:24 +0800

    implementing shuntingyard

--- search.py
+++ search.py
@@ -3,6 +3,9 @@ import re
 import nltk
 import sys
 import getopt
+import pickle
+
+from TermDictionary import TermDictionary
 
 def usage():
     print("usage: " + sys.argv[0] + " -d dictionary-file -p postings-file -q file-of-queries -o output-file-of-results")
@@ -16,9 +19,51 @@ def run_search(dict_file, postings_file, queries_file, results_file):
     # This is an empty method
     # Pls implement your code in below
 
+    termDict = TermDictionary(dict_file)
+    termDict.load()
+
+def splitQuery(query):
+    """
+    Takes in a query string and splits it into an array of query terms and operators,
+    without spaces
+    """
+    temp = nltk.tokenize.word_tokenize(query)
+    stemmer = nltk.stem.porter.PorterStemmer()
+    result = []
+    for term in temp:
+        if not isOperator(term):
+            result.append(stemmer.stem(term.lower()))
+        else:
+            result.append(term)
     
+    return result
+        
+    
+def shuntingYard(query):
+    operatorStack = []
+    output = []
+
+    queryTerms = splitQuery(query)
 
 
+
+
+def evaluateRPN(expression):
+    pass
+
+def isOfGreaterPrecedence(operator1, operator2):
+    operatorPrecedence = {"(": 4, ")" : 4, "NOT" : 3, "AND": 2, "OR" : 1}
+    return operatorPrecedence[operator1] > operatorPrecedence[operator2]
+
+def isOperator(term):
+    operators = ["(", ")", "NOT", "AND", "OR"]
+    return term in operators
+    
+def retrievePostingsList(file, pointer):
+    with open(file, 'rb') as f:
+        f.seek(pointer)
+        # print(f.tell())
+        return pickle.load(f)
 
 dictionary_file = postings_file = file_of_queries = output_file_of_results = None
 
