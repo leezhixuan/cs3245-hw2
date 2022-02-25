47f2839fe5cf69cac709c173d12e44d964f1e2c1
zhixuanlee <overlypositiveplayer@gmail.com>
2022-02-26 00:11:12 +0800

    misc files

--- read.py
+++ read.py
@@ -1,11 +1,20 @@
 import pickle
 
 
-with open("dictionary.txt", "rb") as f:
-    data = pickle.load(f)
-    print(max(data.values()))
-f.close()
+# with open("dictionary.txt", "rb") as f:
+#     data = pickle.load(f)
+#     print(data)
+# #     print(max(data.values()))
+# f.close()
 
-with open("temp.txt", 'rb') as f:
-    data = pickle.load(f)
-    print(data)
+# with open("postings.txt", 'rb') as f:
+#     for i in range(5):
+#         print(pickle.load(f))
+
+
+with open("postings.txt", 'rb') as f:
+    # for i in range(2):
+    f.seek(12724215)
+    result = pickle.load(f)
+f.close()
+print(result)
